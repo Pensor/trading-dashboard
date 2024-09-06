6f959cdaf507c1ca0865cae5c1178066645bf426
Patrick <patrick.feldtmann@gmail.com>
2024-09-06 11:37:55 +0200

    add icons package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,12 +10,7 @@ export default defineNuxtConfig({
   },
   icon: {
     clientBundle: {
-      scan: true,
-      scan: {
-        // note that when you specify those values, the default behavior will be overridden
-        globInclude: ["components/**/*.vue" /* ... */],
-        globExclude: ["node_modules", "dist" /* ... */]
-      }
+      scan: true
     }
   }
 });
