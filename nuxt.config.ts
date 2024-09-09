3d861498446b73e0bd7c06c0ac258352df935bd3
Patrick <patrick.feldtmann@gmail.com>
2024-09-09 15:23:40 +0200

    add algorithm chapter 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/content", "@nuxt/image"],
+
   content: {
     highlight: {
       preload: ["js", "json"],
       theme: "material-theme"
     }
-  }
+  },
+
+  compatibilityDate: "2024-09-09"
 });
